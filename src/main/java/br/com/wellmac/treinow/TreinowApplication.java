3303c4e78193ea579da3f897ad93db52d10b25e0
Wellington.machado <dev.wellmac@gmail.com>
2022-12-04 19:11:55 -0300

    Projeto criado

---
+++ src/main/java/br/com/wellmac/treinow/TreinowApplication.java
@@ -0,0 +1,13 @@
+package br.com.wellmac.treinow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wellmac/treinow/TreinowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wellmac.treinow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
